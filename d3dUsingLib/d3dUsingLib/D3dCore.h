ce3a92286c72c2bdc57e7c07780e48a9276a930b
#1 DC Shin <dcflight@naver.com>
2014-04-03 14:46:01 +0900

    work complete

---
+++ d3dUsingLib/d3dUsingLib/D3dCore.h
@@ -0,0 +1,10 @@
+#pragma once
+class CD3dCore
+{
+public:
+	CD3dCore(void);
+	~CD3dCore(void);
+
+	void Create( int x, int y );
+};
+
---
+++ d3dUsingLib/libImporter/libImporter.cpp
@@ -0,0 +1,14 @@
+// libImporter.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "D3dCore.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	CD3dCore tutor;
+	tutor.Create( 600, 600 );
+
+	return 0;
+}
+
